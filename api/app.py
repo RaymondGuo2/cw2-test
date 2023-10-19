39ad1821a7eaa63ba978f7df579f4965059c21e1
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-10-19 10:32:52 +0100

    Add updated files

--- api/app.py
+++ api/app.py
@@ -2,12 +2,28 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home_page():
     return render_template("index.html")
 
-@app.route("/submit", methods=["POST"])
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
+@app.route("/hat")
+def hat_page():
+    return render_template("hat.html")
+
+@app.route("/jumper")
+def jumper_page():
+    return render_template("jumper.html")
+
+@app.route("/shoes")
+def shoes_page():
+    return render_template("shoes.html")
+
+@app.route("/submit", methods = ["POST"])
 def submit():
     input_name = request.form.get("name")
     input_email = request.form.get("email")
-    input_email = request.form.get("message")
-    return render_template("hello.html", name=input_name, email=input_email, message=input_message)
+    input_message = request.form.get("message")
+    return render_template("form.html", name = input_name, email = input_email, message = input_message)
