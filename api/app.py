b0e032b71c020bd3a0004dfc7f986d45e35674d1
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-10-19 10:56:29 +0100

    Update files

--- api/app.py
+++ api/app.py
@@ -5,22 +5,6 @@ app = Flask(__name__)
 def home_page():
     return render_template("index.html")
 
-@app.route("/contact")
-def contact_page():
-    return render_template("contact.html")
-
-@app.route("/hat")
-def hat_page():
-    return render_template("hat.html")
-
-@app.route("/jumper")
-def jumper_page():
-    return render_template("jumper.html")
-
-@app.route("/shoes")
-def shoes_page():
-    return render_template("shoes.html")
-
 @app.route("/submit", methods = ["POST"])
 def submit():
     input_name = request.form.get("name")
