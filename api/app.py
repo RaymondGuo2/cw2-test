4b8571f49b81dc3ecfa6917320b8872e38d36754
Raymond Guo <raymond.guo23@imperial.ac.uk>
2023-10-23 15:31:20 +0100

    Add yaml

--- api/app.py
+++ api/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route("/")
 def home_page():
     return render_template("index.html")
 
-@app.route("/submit", methods = ["POST"])
+
+@app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_email = request.form.get("email")
     input_message = request.form.get("message")
-    return render_template("form.html", name = input_name, email = input_email, message = input_message)
+    return render_template("form.html", name=input_name,
+                           email=input_email, message=input_message)
